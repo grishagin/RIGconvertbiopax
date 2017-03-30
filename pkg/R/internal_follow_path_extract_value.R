9097485b1de2e8ffd733a243959a73d80b39def2
Ivan Grishagin <Ivan Grishagin>
2017-03-30 15:21:24 -0400

    debugging

--- pkg/R/internal_follow_path_extract_value.R
+++ pkg/R/internal_follow_path_extract_value.R
@@ -144,8 +144,8 @@ internal_follow_path_extract_value<-
             paste(collapse=sep) 
         
         if(exists("complex_name"
-                  ,where=environment()
-                  ,envir=environment()) & 
+                  ,envir=environment()
+                  ,inherits=FALSE) & 
            !is.na(to_return) & 
            !is.null(to_return)){
             to_return<-
