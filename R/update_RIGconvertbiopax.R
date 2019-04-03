90bce8899b8d64f210e2e235965d6a6e9238ef42
Ivan Grishagin <ivan.grishagin@ranchobiosciences.com>
2019-04-03 13:52:02 -0400

    v3.6.5: moved all files into root folder from legacy pkg directory, updated update script accordingly.

---
+++ R/update_RIGconvertbiopax.R
@@ -0,0 +1,15 @@
+update_RIGconvertbiopax <-
+    function () 
+    {
+        #' @title
+        #' Update RIGconvertbiopax from GitHub
+        #' @description 
+        #' Updates the package. It may be necessary to restart the R session after the update.
+        
+        #' @author 
+        #' Ivan Grishagin
+        
+        
+        unloadNamespace(ns = "RIGconvertbiopax")
+        devtools::install_github("grishagin/RIGconvertbiopax")
+    }
